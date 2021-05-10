be68086495adcd1fbd22ce9e82cfe512b0e3210a
Atsea0107 <48303032+Atsea0107@users.noreply.github.com>
2021-05-10 19:49:37 +0800

    create maven project

---
+++ src/main/java/com/zpf/App.java
@@ -0,0 +1,11 @@
+package com.zpf;
+
+/**
+ * @author zpf
+ * @createTime 2021-05-10 19:47
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
