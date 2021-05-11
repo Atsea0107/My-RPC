cd38aa9c7b164d6e4ccfb4f68547366256033f2b
Atsea0107 <48303032+Atsea0107@users.noreply.github.com>
2021-05-11 10:57:09 +0800

    a simple realize

--- test-server/src/main/java/com/zpf/TestService.java
+++ test-server/src/main/java/com/zpf/TestService.java
@@ -12,7 +12,8 @@ public class TestService {
     public static void main(String[] args) {
         // 服务提供方返回的应该是接口
         HelloService helloService = new HelloServiceImpl();
-        // 服务的注册
+        // 服务的注册 注册完服务后，服务器就启动了
+        // 应该将服务器和注册中心解耦
         RpcServer rpcServer = new RpcServer();
         rpcServer.register(helloService, 9000);
     }
